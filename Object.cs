73333488cd7e32dff4af5940a69a5cd414c5f901
The-Glorious-Moron <154225362+The-Glorious-Moron@users.noreply.github.com>
2023-12-18 12:13:24 +0100

    Create Object.cs

---
+++ Object.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+public class GameObject
+{
+    // Example properties for a game object
+    public string Name { get; set; }
+    public Vector3 Position { get; set; }
+    public Vector3 Rotation { get; set; }
+
+    // Update method for each game object
+    public void Update()
+    {
+        // Logic to update the game object
+    }
+}
